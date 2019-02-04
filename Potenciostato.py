97ee49f32c73979f7d70c001628cecf7a946351f
Gabriel Gardin <gabrielgardin96@gmail.com>
2019-02-04 20:42:52 +0000

    Conectei funções aos botões falta fazer o botão stop

--- Potenciostato.py
+++ Potenciostato.py
@@ -1,18 +1,23 @@
 
 from PyQt5 import uic, QtGui
-from PyQt5.QtWidgets import QApplication, QMessageBox
+from PyQt5.QtWidgets import QApplication, QMessageBox, QFileDialog
 from functools import partial
 import sys
+import csv
 from voltLinear import LinearVoltametry
 from voltCyclic import CyclicVoltametry
 from voltSquare import SquareWaveVoltametry
 import pyqtgraph as pg
 from PreDeposition import PreDeposition
 
+pg.setConfigOption('background', 'w')
+
 form_main, base_main = uic.loadUiType('/home/pi/Desktop/PotenciosPi/main_window.ui')
 form_linear, base_linear = uic.loadUiType('/home/pi/Desktop/PotenciosPi/Linear_window.ui')
 form_cyclic, base_cyclic = uic.loadUiType('/home/pi/Desktop/PotenciosPi/Cyclic_window.ui')
 form_SQW, base_SQW = uic.loadUiType('/home/pi/Desktop/PotenciosPi/SQW_window.ui')
+
+started = True
 
 class main_window(form_main, base_main):
     def __init__(self):
@@ -43,44 +48,55 @@ class Linear_window(form_linear, base_linear):
         self.actionLinear_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='linear'))
         self.actionCyclic_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='cyclic'))
         self.actionSquare_Wave_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='sqw'))
+        self.pushButton.clicked.connect(stop_voltamogram)
+        self.actionSave.triggered.connect(partial(save, obj=self))
         self.pushButton_2.clicked.connect(self.run_linear)
         self.actionExit.triggered.connect(self.close)
+        self.lineAcqPoint.setText("300")
+        self.lineDelayPoint.setText("100")
+        self.lineVoltScans.setText("1")
+        self.lineVoltScan.setText("50")
+        self.lineVoltStep.setText("10")
+        self.lineVoltFinal.setText("600")
+        self.lineVoltInitia.setText("0")
 
     def run_linear(self):
-        if (self.checkBox.isChecked()):
-            preCond = int(self.linePreCond.text())
-            pre_time_cond = int(self.linePretime.text())
-            pot_pre_dep = int(self.linePreDep.text())
-            pre_deposition_time = int(self.linePreDepTime.text())
+        #self.pushButton_2.disconnect()
+        #self.pushButton_2.clicked.connect(partial(stop_voltamogram, obj=self))
+        while(started == True):
+            if (self.checkBox.isChecked()):
+                preCond = int(self.linePreCond.text())
+                pre_time_cond = int(self.linePretime.text())
+                pot_pre_dep = int(self.linePreDep.text())
+                pre_deposition_time = int(self.linePreDepTime.text())
 
-            i2 = PreDeposition(pot_cond=preCond, time_cond=pre_time_cond, pre_dep_pot=pot_pre_dep,
-                               pre_dep_time=pre_deposition_time, somadorDA=3.0)
-            i2.run()
+                i2 = PreDeposition(pot_cond=preCond, time_cond=pre_time_cond, pre_dep_pot=pot_pre_dep,
+                                pre_dep_time=pre_deposition_time, somadorDA=3.0)
+                i2.run()
 
-        print(self.lineVoltInitia.text())
-        potInii = int(self.lineVoltInitia.text())
-        potFinn = int(self.lineVoltFinal.text())
-        potStepp = int(self.lineVoltStep.text())
-        potScann = int(self.lineVoltScan.text())
-        potScanss = int(self.lineVoltScans.text())
-        acq_pointss = int(self.lineAcqPoint.text())
-        delay_pointss = int(self.lineDelayPoint.text())
-        ganhoo = int(self.spinBox.text())
+            print(self.lineVoltInitia.text())
+            potInii = int(self.lineVoltInitia.text())
+            potFinn = int(self.lineVoltFinal.text())
+            potStepp = int(self.lineVoltStep.text())
+            potScann = int(self.lineVoltScan.text())
+            potScanss = int(self.lineVoltScans.text())
+            acq_pointss = int(self.lineAcqPoint.text())
+            delay_pointss = int(self.lineDelayPoint.text())
+            ganhoo = int(self.spinBox.text())
 
-        i1 = LinearVoltametry(dac_sum=3.0, acq_points=acq_pointss, delay_points=delay_pointss,
-                              potIni=potInii, potFin=potFinn, stepVolt=potStepp, ganho=ganhoo, scanRate=potScann)
+            i1 = LinearVoltametry(dac_sum=3.0, acq_points=acq_pointss, delay_points=delay_pointss,
+                                potIni=potInii, potFin=potFinn, stepVolt=potStepp, ganho=ganhoo, scanRate=potScann)
 
-        Ydata = []
-        Xdata = []
-
-        for x in i1.run():
-            Xdata.append(x[0])
-            Ydata.append(x[1])
-            print(Xdata[0],Ydata[0])
-            self.textBrowser.append(str([x[0],x[1]]))
-            self.graphicsView.plot(Xdata, Ydata, clear=True)
-            QtGui.QApplication.processEvents()
-            print([x[0],x[1]])
+            self.Xdata = []
+            self.Ydata = []
+            for x in i1.run():
+                self.Xdata.append(x[0])
+                self.Ydata.append(x[1])
+                print(self.Xdata[0],self.Ydata[0])
+                self.textBrowser.append(str([x[0],x[1]]))
+                self.graphicsView.plot(self.Xdata, self.Ydata, clear=True)
+                QtGui.QApplication.processEvents()
+                print([x[0],x[1]])
 
     def closeEvent(self, event):
         msg = QMessageBox()
@@ -99,7 +115,51 @@ class Cyclic_window(form_cyclic, base_cyclic):
         self.actionLinear_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='linear'))
         self.actionCyclic_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='cyclic'))
         self.actionSquare_Wave_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='sqw'))
+        self.actionSave.triggered.connect(partial(save, obj=self))
+        self.pushButton_2.clicked.connect(self.run_cyclic)
         self.actionExit.triggered.connect(self.close)
+        self.lineAcqPoint.setText("300")
+        self.lineDelayPoint.setText("100")
+        self.lineVoltScans.setText("1")
+        self.lineVoltScan.setText("50")
+        self.lineVoltStep.setText("10")
+        self.lineVoltFinal.setText("600")
+        self.lineVoltInitia.setText("0")
+
+    def run_cyclic(self):
+        if(self.checkBox.isChecked()):
+            preCond = int(self.linePreCond.text())
+            pre_time_cond = int(self.linePretime.text())
+            pot_pre_dep = int(self.linePreDep.text())
+            pre_deposition_time = int(self.linePreDepTime.text())
+
+            i2 = PreDeposition(pot_cond = preCond, time_cond = pre_time_cond, pre_dep_pot=pot_pre_dep, pre_dep_time = pre_deposition_time, somadorDA = 3.0)
+            i2.run()
+
+        print(self.lineVoltInitia.text())
+        potInii = int(self.lineVoltInitia.text())
+        potFinn = int(self.lineVoltFinal.text())
+        potStepp = int(self.lineVoltStep.text())
+        potScann = int(self.lineVoltScan.text())
+        potScanss = int(self.lineVoltScans.text())
+        acq_pointss = int(self.lineAcqPoint.text())
+        delay_pointss = int(self.lineDelayPoint.text())
+        ganhoo = int(self.spinBox.text()) 
+
+
+        i1 = CyclicVoltametry(dac_sum=3.0,acq_points=acq_pointss,delay_points=delay_pointss,
+                         potIni=potInii,potFin=potFinn,stepVolt=potStepp,ganho=ganhoo,scanRate=potScann)
+
+        self.Ydata = []
+        self.Xdata = []
+        for x in i1.run():
+            self.Xdata.append(x[0])
+            self.Ydata.append(x[1])
+            # print(data[0],data[1])
+            self.textBrowser.append(str([x[0], x[1]]))
+            self.graphicsView.plot(self.Xdata, self.Ydata, clear=True)
+            QtGui.QApplication.processEvents()
+            print([x[0], x[1]])
 
     def closeEvent(self, event):
         msg = QMessageBox()
@@ -118,7 +178,49 @@ class SQW_window(form_SQW,base_SQW):
         self.actionLinear_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='linear'))
         self.actionCyclic_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='cyclic'))
         self.actionSquare_Wave_Voltametry.triggered.connect(partial(slct_GUI, obj=self, gui='sqw'))
+        self.actionSave.triggered.connect(partial(save, obj=self))
+        self.pushButton_2.triggered.connect(self.run_SQW)
         self.actionExit.triggered.connect(self.close)
+        self.lineAcqPoint.setText("300")
+        self.lineDelayPoint.setText("100")
+        self.lineVoltScans.setText("1")
+        self.lineVoltScan.setText("50")
+        self.lineVoltStep.setText("10")
+        self.lineVoltFinal.setText("600")
+        self.lineVoltInitia.setText("0")
+
+    def run_SQW(self):
+        if(self.checkBox.isChecked()):
+            preCond = int(self.linePreCond.text())
+            pre_time_cond = int(self.linePretime.text())
+            pot_pre_dep = int(self.linePreDep.text())
+            pre_deposition_time = int(self.linePreDepTime.text())
+
+            i2 = PreDeposition(pot_cond = preCond, time_cond = pre_time_cond, pre_dep_pot=pot_pre_dep, pre_dep_time = pre_deposition_time, somadorDA = 3.0)
+            i2.run()
+        print(self.lineVoltInitia.text())
+        potInii = int(self.lineVoltInitia.text())
+        potFinn = int(self.lineVoltFinal.text())
+        potStepp = int(self.lineVoltStep.text())
+        potScann = int(self.lineVoltScan.text())
+        potScanss = int(self.lineVoltScans.text())
+        acq_pointss = int(self.lineAcqPoint.text())
+        delay_pointss = int(self.lineDelayPoint.text())
+        ganhoo = int(self.spinBox.text()) 
+        
+        i1 = SquareWaveVoltametry(dac_sum=3.0,acq_points=acq_pointss,delay_points=delay_pointss,
+                         potIni=potInii,potFin=potFinn,stepVolt=potStepp,ganho=ganhoo,ampP=50,freq=10)
+
+        self.Ydata = []
+        self.Xdata = []
+        for x in i1.run():
+            self.Xdata.append(x[0])
+            self.Ydata.append(x[1])
+            # print(data[0],data[1])
+            self.textBrowser.append(str([x[0], x[1]]))
+            self.graphicsView.plot(self.Xdata, self.Ydata, clear=True)
+            QtGui.QApplication.processEvents()
+            print([x[0], x[1]])
 
     def closeEvent(self, event):
         msg = QMessageBox()
@@ -130,6 +232,19 @@ class SQW_window(form_SQW,base_SQW):
         else:
             event.ignore()
 
+def stop_voltamogram(obj):
+    global started
+    if(started):
+        started = False
+        print("pedi pra parar parou")
+
+
+def save(obj):
+    name = QFileDialog.getSaveFileName(obj, 'Save File')
+    with open(name[0]+'.csv', 'w') as voltametry_data:
+        writer = csv.writer(voltametry_data, delimiter=',')
+        for i in range(len(obj.Ydata)):
+            writer.writerow([obj.Xdata[i], obj.Ydata[i]])
 
 
 def slct_GUI(obj, gui):
